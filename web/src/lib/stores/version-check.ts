39f508696ba5cb9a8f411468fab4eec3ebcca458
Arben Apura <arbenapura.official@gmail.com>
2026-09-09 11:04:36 +0800

    chore(release): bump version to 0.5.0-beta.5 and extension to 1.2.0-beta.5

--- web/src/lib/stores/version-check.ts
+++ web/src/lib/stores/version-check.ts
@@ -24,7 +24,7 @@ const STORAGE_KEY_PREFIX = 'xianscan_dismissed_update_';
 const initialState: VersionCheckState = {
 	checking: false,
 	hasUpdate: false,
-	currentVersion: '0.5.0-beta.4',
+	currentVersion: '0.5.0-beta.5',
 	webBuildHash: 'dev',
 	latestVersion: null,
 	releaseUrl: null,
