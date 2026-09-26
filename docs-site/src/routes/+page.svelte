6298bdad898527399bb4f2a3668638a7d4c02ac7
Arben Apura <arbenapura.official@gmail.com>
2026-09-26 15:51:45 +0800

    chore(release): bump version to 0.5.0-beta.8

--- docs-site/src/routes/+page.svelte
+++ docs-site/src/routes/+page.svelte
@@ -235,7 +235,7 @@
 		<div class="flex flex-col items-center text-center">
 			<div class="mb-5 flex items-center justify-center">
 				<Badge variant="cinnabar">
-					v0.5.0-beta.7 Native Engine
+					v0.5.0-beta.8 Native Engine
 				</Badge>
 			</div>
 
